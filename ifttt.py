dc55c82a480a1164450fc2996693008c3f2929d8
abhishekarora <faridabad.abhishek@gmail.com>
2021-07-19 21:28:41 +0530

    ok

--- ifttt.py
+++ ifttt.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 
-print "This is the name of the file: ", sys.argv[1]
+print "This is the name of the file: ", sys.argv[1] 
+
 
 requests.post("https://maker.ifttt.com/trigger/front_door_movement/with/key/yourkey", data="")
